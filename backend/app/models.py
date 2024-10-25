61d1512df34b60ae353bcca4204a33425ee54602
tadashi <tahofu89@gmail.com>
2024-10-25 16:36:48 +0900

    DBをmySQLに変更しました

--- backend/app/models.py
+++ backend/app/models.py
@@ -9,14 +9,14 @@ class Match(Base):
     __tablename__ = "matches"
 
     id = Column(Integer, primary_key=True, index=True)
-    home_team_uid = Column(String)
-    away_team_uid = Column(String)
+    home_team_uid = Column(String(255))
+    away_team_uid = Column(String(255))
     year = Column(Integer)
     month = Column(Integer)
     day = Column(Integer)
-    start_time = Column(String)
-    end_time = Column(String)
-    location = Column(String)
+    start_time = Column(String(10))
+    end_time = Column(String(10))
+    location = Column(String(20))
 
 
 # Team
@@ -24,12 +24,12 @@ class Team(Base):
     __tablename__ = "teams"
 
     id = Column(Integer, primary_key=True, index=True)
-    uid = Column(String)
-    name = Column(String)
-    region = Column(String)
-    prefecture = Column(String)
-    category = Column(String)
-    league = Column(String)
+    uid = Column(String(255))
+    name = Column(String(255))
+    region = Column(String(10))
+    prefecture = Column(String(10))
+    category = Column(String(10))
+    league = Column(String(20))
 
 
 # Player
@@ -37,14 +37,14 @@ class Player(Base):
     __tablename__ = "players"
 
     id = Column(Integer, primary_key=True)
-    uid = Column(String)
+    uid = Column(String(255))
     position = Column(Enum("GK", "DF", "MF", "FW", name="positions"))
     number = Column(Integer)
-    namae = Column(String)
-    name = Column(String)
+    namae = Column(String(20))
+    name = Column(String(20))
     height = Column(Integer)
     weight = Column(Integer)
-    previous_team = Column(String)
+    previous_team = Column(String(20))
 
 
 # Recruitment
@@ -52,14 +52,17 @@ class Recruitment(Base):
     __tablename__ = "recruitments"
 
     id = Column(Integer, primary_key=True, index=True)
-    uid = Column(String)
-    status = Column(Enum("募集中", "申し込み受領", "マッチ済み", name="Recruitment_status"), default="募集中")
+    uid = Column(String(255))
+    status = Column(
+        Enum("募集中", "申し込み受領", "マッチ済み", name="Recruitment_status"),
+        default="募集中",
+    )
     year = Column(Integer)
     month = Column(Integer)
     day = Column(Integer)
-    start_time = Column(String)
-    end_time = Column(String)
-    location = Column(String)
+    start_time = Column(String(10))
+    end_time = Column(String(10))
+    location = Column(String(20))
 
     applications = relationship("Application", back_populates="recruitment")
 
@@ -70,7 +73,9 @@ class Application(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     recruitment_id = Column(Integer, ForeignKey("recruitments.id"))
-    uid = Column(String)
-    status = Column(Enum("回答待ち", "承認", "辞退", name="application_status"), default="回答待ち")
+    uid = Column(String(255))
+    status = Column(
+        Enum("回答待ち", "承認", "辞退", name="application_status"), default="回答待ち"
+    )
 
     recruitment = relationship("Recruitment", back_populates="applications")
