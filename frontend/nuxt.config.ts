91f722c0fefe3286446d5581a05efed3e99f77a1
tadashi <tahofu89@gmail.com>
2024-10-29 02:06:57 +0900

    configを修正しました

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -34,13 +34,11 @@ export default defineNuxtConfig({
             enabled: true,
         },
         config: {
+            projectName: process.env.PROJECT_NAME,
+            projectId: process.env.PROJECT_ID,
+            projectNumber: process.env.PROJECT_NUMBER,
             apiKey: process.env.API_KEY,
             authDomain: process.env.AUTH_DOMAIN,
-            projectId: process.env.PROJECT_ID,
-            storageBucket: process.env.STORAGE_BUCKET,
-            messagingSenderId: process.env.MESSAGING_SENDER_ID,
-            appId: process.env.APP_ID,
-            measurementId: process.env.MEASUREMENT_ID,
         },
     },
 });
