ff73f08f5ecac58109566b2d7cc5caba126986a2
tadashi <tahofu89@gmail.com>
2024-10-29 03:00:52 +0900

    /を追加しました

--- frontend/middleware/auth.global.ts
+++ frontend/middleware/auth.global.ts
@@ -2,7 +2,7 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
     const user = await getCurrentUser();
 
     // redirect the user to the login page
-    if (!user && to.path !== '/login') {
-        return navigateTo({ path: '/login' });
+    if (!user && to.path !== '/login/') {
+        return navigateTo({ path: '/login/' });
     }
 });
