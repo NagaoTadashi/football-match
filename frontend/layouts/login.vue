4c4fe8e6944a3a300480e52e004af8e15ce13e14
tadashi <tahofu89@gmail.com>
2024-10-29 20:50:57 +0900

    UIを変更しました

--- frontend/layouts/login.vue
+++ frontend/layouts/login.vue
@@ -1,6 +1,31 @@
+<script setup>
+import { ref } from 'vue';
+
+const visible = ref(false);
+</script>
+
 <template>
     <div>
-        <p>チーム用のgoogleアカウントを作成してログインしてください</p>
-        <slot />
+        <div>
+            <div class="text-center mx-auto my-6" max-width="228" text>
+                <h1><v-icon>mdi-soccer</v-icon> Football Match</h1>
+            </div>
+
+            <v-card
+                class="mx-auto pa-12 pb-8"
+                elevation="8"
+                max-width="448"
+                rounded="lg"
+            >
+                <v-card class="mb-12" color="surface-variant" variant="tonal">
+                    <v-card-text class="text-medium-emphasis text-caption">
+                        <v-icon>mdi-information-outline</v-icon>
+                        チームで1つのGoogleアカウントを用意してください
+                    </v-card-text>
+                </v-card>
+
+                <slot />
+            </v-card>
+        </div>
     </div>
 </template>
